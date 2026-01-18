f6fe320dbe1f91d7d344feb9f8ee97581ff9729a
Siddharth Jaswal <sidatwork123@gmail.com>
2026-01-18 19:06:31 +0530

    fixed build

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,6 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=False)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
